993a2f0eb3fcb2382c74a0f9a6de0a3615ed3603
joelschum@gmail.com <joelschum@gmail.com>
2015-01-03 17:55:34 +0100

    Initial commit

---
+++ lush.lua
@@ -0,0 +1,102 @@
+do
+	
+local soundDataMap = {}
+local getSoundData = function(str)
+	if not soundDataMap[str] then
+		soundDataMap[str] = love.sound.newSoundData(str)
+		return soundDataMap[str]
+	else
+		return soundDataMap[str]
+	end
+end
+
+lush = {} -- Lightweight, Unefficient Sound Helper library
+local sources = {}
+
+local transformTagList = function(tags)
+	local ret = {}
+	for tagIndex = 1, #tags do
+		ret[tags[tagIndex]] = true
+	end
+	return ret
+end
+	
+function lush.play(filename, properties)
+	if type(filename) == "table" then filename = filename[love.math.random(1,#filename)] end
+	
+	properties = properties and properties or {}
+	properties.looping = properties.looping and properties.looping or false
+	properties.stream = properties.stream and properties.stream or false
+	properties.volume = properties.volume and properties.volume or 1.0
+	properties.tags = properties.tags and transformTagList(properties.tags) or {}
+	properties.tags["all"] = true
+	
+	local src
+	if properties.stream == true then
+		src = love.audio.newSource(filename, "stream")
+	else
+		src = love.audio.newSource(getSoundData(filename))
+	end
+	
+	src:setLooping(properties.looping)
+	src:setVolume(properties.volume)
+	love.audio.play(src)
+	
+	table.insert(sources, {source = src, properties = properties})
+	return src
+end
+
+function lush.getSourceProperties(src)
+	for i = 1, #sources do
+		if sources[i].source == src then return sources[i].properties end
+	end
+end
+	
+function lush.update() 
+	-- Benchmark this and see how many elements are removed each update. If the number is big enough, consider setting the elements to remove to nil
+	-- and shifting the array by myself: http://stackoverflow.com/questions/12394841/safely-remove-items-from-an-array-table-while-iterating
+	--local useNow = [[
+	for i = #sources, 1, -1 do
+		if sources[i].source:isStopped() then
+			table.remove(sources, i) 
+		end
+	end
+	--]]
+	
+	-- method state above is implemented here for later comparison:
+	local BenchmarkLater = [[
+	local n = #sources
+	for i = 1, n do
+		if sources[i].source:isStopped() then sources[i] = nil end
+	end
+	
+	local newLength = 0
+	for i = 1, n do
+		if sources[i] then
+			newLength = newLength + 1
+			sources[newLength] = sources[i]
+		end
+	end
+	
+	for i = newLength + 1, n do
+		sources[i] = nil
+	end
+	]]
+end
+
+function lush.actTag(tags, func)
+	for ti = 1, #tags do
+		for si = 1, #sources do
+			if sources[si].properties.tags[tags[ti]] then
+				func(sources[si].source)
+			end
+		end
+	end
+end
+
+function lush.tagPlay(tags) lush.actTag(tags, function(source) source:play() end) end
+function lush.tagStop(tags) lush.actTag(tags, function(source) source:stop() end) end
+function lush.tagPause(tags) lush.actTag(tags, function(source) source:pause() end) end
+function lush.tagSetVolume(tags, volume) lush.actTag(tags, function(source) source:setVolume(volume) end) end
+
+end
---
+++ main.lua
@@ -0,0 +1,29 @@
+require "lush"
+
+function love.load()
+	bgMusic = lush.play("jingle.ogg", {tags = {"bgmusic"}, looping = true, stream = true})
+	nextShot = 0
+end
+
+function love.update()
+	if nextShot < love.timer.getTime() then
+		nextShot = love.timer.getTime() + 0.5
+		lush.play({"1.wav", "2.wav"})
+	end
+	
+	lush.tagSetVolume({"all"}, 1.0)
+	
+	if love.keyboard.isDown(" ") then
+		lush.tagSetVolume({"all"}, 0.1)
+	end
+	
+	if love.keyboard.isDown("lctrl") then
+		lush.tagSetVolume({"bgmusic"}, 0.1)
+	end
+	
+	if love.keyboard.isDown("s") then
+		lush.tagStop({"all"})
+	end
+	
+	lush.update()
+end
