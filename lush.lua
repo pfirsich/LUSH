8176b45df032c9716abad8bef89be75ca4810d40
joelschum@gmail.com <joelschum@gmail.com>
2015-03-11 00:43:58 +0100

    Code review.

--- lush.lua
+++ lush.lua
@@ -1,5 +1,5 @@
 do
-	
+
 local soundDataMap = {}
 local getSoundData = function(str)
 	if not soundDataMap[str] then
@@ -17,8 +17,8 @@ local defaultVolume = 1.0
 
 local transformTagList = function(tags)
 	local ret = {}
-	for tagIndex = 1, #tags do
-		ret[tags[tagIndex]] = true
+	for i = 1, #tags do
+		ret[tags[i]] = true
 	end
 	return ret
 end
@@ -26,81 +26,72 @@ end
 function lush.setPath(p) path = p end
 
 function lush.setDefaultVolume(vol) defaultVolume = vol end
-	
+
 function lush.play(dataSource, properties)
 	properties = properties or {}
-	properties.looping = properties.looping or false
-	properties.stream = properties.stream or false
-	properties.volume = properties.volume or defaultVolume
-	properties.tags = properties.tags and transformTagList(properties.tags) or {}
-	properties.tags["all"] = true
-	
+	local tags = properties.tags and transformTagList(properties.tags) or {}
+	tags["all"] = true
+
 	local src
 	if type(dataSource) == "userdata" and dataSource:typeOf("SoundData") then
 		src = love.audio.newSource(dataSource)
 	else
 		if type(dataSource) == "table" then dataSource = dataSource[love.math.random(1,#dataSource)] end
 		dataSource = path .. dataSource
-		
+
 		if properties.stream == true then
 			src = love.audio.newSource(dataSource, "stream")
 		else
 			src = love.audio.newSource(getSoundData(dataSource))
 		end
 	end
-	
-	src:setLooping(properties.looping)
-	src:setVolume(properties.volume)
-	if properties.pitchShift then
-		src:setPitch(properties.pitchShift + 1.0)
-	end
+
+	src:setLooping(properties.looping or false)
+	src:setVolume(properties.volume or defaultVolume)
+	src:setPitch(1.0 + (properties.pitchShift or 0.0))
 	love.audio.play(src)
-	
-	table.insert(sources, {source = src, properties = properties})
+
+	table.insert(sources, {source = src, tags = tags})
 	return src
 end
 
-function lush.getSourceProperties(src)
+function lush.getSourceTags(src)
 	for i = 1, #sources do
-		if sources[i].source == src then return sources[i].properties end
+		if sources[i].source == src then return sources[i].tags end
 	end
 end
-	
-function lush.update() 
+
+function lush.update()
 	-- Benchmark this and see how many elements are removed each update. If the number is big enough, consider using the version in "BenchmarkLater"
-	--local useNow = [[
 	for i = #sources, 1, -1 do
 		if sources[i].source:isStopped() then
-			table.remove(sources, i) 
+			table.remove(sources, i)
 		end
 	end
-	--]]
-	
+
 	-- method state above is implemented here for later comparison:
-	local BenchmarkLater = [[
-	local n = #sources
-	for i = 1, n do
-		if sources[i].source:isStopped() then sources[i] = nil end
-	end
-	
-	local newLength = 0
-	for i = 1, n do
-		if sources[i] then
-			newLength = newLength + 1
-			sources[newLength] = sources[i]
-		end
-	end
-	
-	for i = newLength + 1, n do
-		sources[i] = nil
-	end
-	]]
+	-- local n = #sources
+	-- for i = 1, n do
+	-- 	if sources[i].source:isStopped() then sources[i] = nil end
+	-- end
+	--
+	-- local newLength = 0
+	-- for i = 1, n do
+	-- 	if sources[i] then
+	-- 		newLength = newLength + 1
+	-- 		sources[newLength] = sources[i]
+	-- 	end
+	-- end
+	--
+	-- for i = newLength + 1, n do
+	-- 	sources[i] = nil
+	-- end
 end
 
 function lush.actTag(tags, func)
 	for ti = 1, #tags do
 		for si = 1, #sources do
-			if sources[si].properties.tags[tags[ti]] then
+			if sources[si].tags[tags[ti]] then
 				func(sources[si].source)
 			end
 		end
